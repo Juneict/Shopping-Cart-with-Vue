c10a02cea918a850a862c4be8fab26942ee68121
Juneict <junemoenyinyiict@gmail.com>
2022-08-25 13:32:06 +0630

    shopping cart with vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
+  <header>
+    
+    <span class="cart-item">({{cart.length}})</span> items in Cart
+    
+  </header>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/products" @addItemToCart="addItemToCart" :cart="cart">View Product</router-link> |
+    <router-link to="/cart" :cart="cart">View Cart</router-link>
   </nav>
-  <router-view/>
+  <router-view :cart="cart"></router-view>
+  
 </template>
+<script>
+  import Products from './components/Products.vue'
 
+  export default{
+    data(){
+      return{
+        "page":true,
+        cart:[],
+      }
+    },
+    components:{Products},
+    props:['products'],
+    methods:{
+      addItemToCart(product){
+        this.cart.push(product)
+      }
+    } 
+  }
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,9 +46,24 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
 }
+header{
+  height: 40px;
+  background-color: #2c3e50;
+  color: #ffff;
+  text-align: center;
+  padding-top:15px ;
+}
+.button{
+  background-color: #999;
+  color: #fff;
+  border: none;
+  text-decoration: none;
+}
+
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Cart</h1>
+   <div class="product">
+      
+        <table class="table">
+            <tr>
+                <th>Item</th>
+                <th>Unit</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Action</th>
+            </tr>
+            
+            <tr v-for="(product,index) in cart" :key="index">
+                <td>{{ product.name }}</td>
+                <td>Pc(s)</td>
+                <td>1</td>
+                <td>{{product.price}}</td>
+                <td><button @click="removeCartItem(product)">X</button></td>
+            </tr>
+           
+        </table>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['cart'],
+    methods:{
+        removeCartItem(product){
+            this.cart.splice(this.cart.indexOf(product),1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.table{
+    width: 100%;
+    padding: 10px;
+    color: #999;
+}
+th{
+    font-size: 20px;
+    border-bottom: 1px solid white;
+    color: black;
+    margin-bottom: 3px;  
+}
+button{
+    width: 30px;
+    height: 20px;
+    font-size:10px;
+    text-align: center;
+    padding: 10px;
+    background-color: #ab2626;
+    border-radius: 5px;
+    color: #fff;
+}
+</style>
